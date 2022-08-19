dc3ef824507ad809cf292c8e0f4ecb809891e74b
Konstantin Denerz <konstantin.denerz@gmail.com>
2022-08-19 15:17:49 +0200

    samples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, UntypedFormControl, UntypedFormGroup} from '@angular/forms';
+
+interface Bar {
+  test: FormControl<string | null>;
+}
+
+interface Foo {
+  foo: FormControl<string>;
+  bar?: FormGroup<Bar>;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'labs-angular-typed-forms-sample';
+  readonly untypedFormGroup = new UntypedFormGroup({
+    foo: new UntypedFormControl('foo'),
+    bar: new FormGroup({
+      test: new FormControl('bubu')
+    })
+  })
+
+  readonly typedFormGroup = new FormGroup<Foo>({
+    foo: new FormControl('foo2', {nonNullable: true}),
+    bar: new FormGroup({
+      test: new FormControl('bubu2')
+    })
+  })
+
+  constructor() {
+    console.log('untyped.test.value', (this.untypedFormGroup.controls['bar'] as FormGroup).controls['test'].value)
+    console.log('typed.test.value', this.typedFormGroup.controls.bar?.controls.test.value);
+    this.typedFormGroup.removeControl('bar');
+    console.log('untyped.value', this.untypedFormGroup.value.bar?.test);
+    console.log('typed.value', this.typedFormGroup.value.bar?.test);
+  }
 }
